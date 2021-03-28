5fed8c7060dd6b02689a90e2de38ace3c63c229c
Michael Holmes <mholmes5@asu.edu>
2021-03-27 20:53:33 -0500

    Update gitignore and add environment variables

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -14,16 +15,16 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-const url = "mongodb://localhost:27017/userDB";
-mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology:true});
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser:true, useUnifiedTopology:true});
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-const secret = "Slydoesnotlikewalks.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
