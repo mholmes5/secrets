f8c76e7b5d0e3f0344ed885befc196a612dc1095
Michael Holmes <mholmes5@asu.edu>
2021-04-03 14:41:41 -0500

    Add submit secrets functionality

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -99,13 +100,43 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/secrets', (req, res) => {
-  if(req.isAuthenticated()){
-    res.render('secrets');
-  }else{
-    res.redirect('/login');
-  }
+  User.find({"secret": {$ne:null}}, (err,foundUsers)=>{
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render('secrets', {usersWithSecrets: foundUsers});
+      }
+    }
+  });
 
 });
+
+app.route('/submit')
+  .get((req, res) => {
+    if(req.isAuthenticated()){
+      res.render('submit');
+    }else{
+      res.redirect('/login');
+    }
+
+  })
+
+  .post((req,res) => {
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, foundUser){
+      if(err){
+        console.log(err);
+      }else{
+        if(foundUser){
+          foundUser.secret = submittedSecret;
+          foundUser.save(function(){
+            res.redirect("/secrets");
+          });
+        }
+      }
+    });
+  });
 
 app.get('/logout', (req, res) => {
   req.logout();
